ad2ab21b5013cf8425983a722c4fe8eacf127866
Ian_França <iiann_2009@hotmail.com>
2023-04-27 18:17:09 -0300

    adicionado o componente header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './style.scss'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div>
+                    <img src="/src/assets/images/logo.svg" alt="livro" />
+                </div>
+                <div>
+                    <h2></h2>
+                    <span></span>
+                    <img src="/src/assets/images/icon-moon.svg" alt="" />
+                </div>
+            </header>
+        </>
+    )
+}
+export default Header
