268fd66042a269034a86bf346c94e16a3817f19c
Ian_França <iiann_2009@hotmail.com>
2023-05-14 12:33:53 -0300

    atualizado estrutura e logica do componente WordPanel

--- src/components/WordPanel/WordPanel.jsx
+++ src/components/WordPanel/WordPanel.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import './style.scss'
 
 export default function WordPanel({ keyWord }) {
+
+    console.log()
+
     return (
         <div className='word-panel'>
             <div>
                 <h1>{keyWord?.word}</h1>
                 <p>{keyWord?.phonetics[1]?.text}</p>
             </div >
-            <div>
-                <img src="/src/assets/images/icon-play.svg" alt="Play" />
+            <div className='play'>
+                <img src="/src/assets/images/icon-play.svg" alt="Play" onClick={() => { document.getElementById("audio-play").play() }} />
+                <audio id="audio-play" src={keyWord.phonetics[0].audio} ></audio>
             </div>
         </div>
     )
