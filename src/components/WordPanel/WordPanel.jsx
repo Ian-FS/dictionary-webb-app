9795592f57f05c2248a85b5756d268f29d6801c1
Ian_França <iiann_2009@hotmail.com>
2023-05-16 09:58:54 -0300

    adicionado lógica de tratamento de dados do componente wordPanel

--- src/components/WordPanel/WordPanel.jsx
+++ src/components/WordPanel/WordPanel.jsx
@@ -3,17 +3,20 @@ import './style.scss'
 
 export default function WordPanel({ keyWord }) {
 
-    console.log()
+
+    const newPhonetics = keyWord.phonetics.filter(phonetic => phonetic.audio !== '')
 
     return (
         <div className='word-panel'>
             <div>
                 <h1>{keyWord.word}</h1>
-                <p>{keyWord.phonetics[1].text}</p>
+                {(newPhonetics[2] === undefined) && <p>{newPhonetics[1]?.text}</p>}
+                {(newPhonetics[2] !== undefined) && <p>{newPhonetics[2]?.text}</p>}
             </div >
             <div className='play'>
                 <img src="/src/assets/images/icon-play.svg" alt="Play" onClick={() => { document.getElementById("audio-play").play() }} />
-                <audio id="audio-play" src={keyWord.phonetics[0].audio} ></audio>
+                {(newPhonetics.length < 3 && newPhonetics[1] !== undefined) && <audio id="audio-play" src={newPhonetics[1]?.audio} ></audio>}
+                {(newPhonetics[2] !== undefined) && <audio id="audio-play" src={newPhonetics[2]?.audio} ></audio>}
             </div>
         </div>
     )
