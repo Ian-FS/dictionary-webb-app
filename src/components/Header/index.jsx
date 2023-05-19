4c54ce66a090c5aa3c7b6e0791ea9e77bf1e448d
Ian_França <iiann_2009@hotmail.com>
2023-05-18 23:26:17 -0300

    atualizado caminho do arquivo svg do componente Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,12 +11,12 @@ function Header() {
                 <div className="right-head">
                     <div className="font-type">
                         <h2>Sans Serif</h2>
-                        <img src="./src/assets/images/icon-arrow-down.svg" alt="seta para baixo" />
+                        <img src="/src/assets/images/icon-arrow-down.svg" alt="seta para baixo" />
                     </div>
                     <div className="line-vertical"></div>
                     <div className="mode-theme">
                         <div><i></i></div>
-                        <img src="./src/assets/images/icon-moon.svg" alt="" />
+                        <img src="/home/ians/dictionary-web-app/src/assets/images/icon-moon.svg" alt="" />
                     </div>
 
                 </div>
