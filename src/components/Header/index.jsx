936ff6103642638eb5cfa82ca33833c8cac3f0cd
Ian_França <iiann_2009@hotmail.com>
2023-06-19 20:21:21 -0300

    atualizado lógica e estilo do modo dark tema

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -32,8 +32,18 @@ function Header({ setFont }) {
                         <div onClick={(event) => {
                             event.target.lastChild.classList.toggle("i-toggle")
                             event.target.classList.toggle("div-toggle")
+                            event.target.nextSibling.classList.toggle("img-toggle")
+                            console.log(event.target.classList.toggle("div-toggle"))
+                            if (event.target.classList.toggle("div-toggle")) {
+                                document.documentElement.style.setProperty('--background-primary', '#050505');
+                                document.documentElement.style.setProperty('--background-secondary', '#1f1f1f');
+                                document.documentElement.style.setProperty('--color-primary', '#ffffff');
+                                document.documentElement.style.setProperty('--line-backgroundColor', '#3a3a3a');
+                            }
+
                         }}><i></i></div>
-                        <img src={moonSvg} alt="" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"><path fill="none" stroke="#838383" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z" /></svg>
+                        {/* <img src={moonSvg} alt="" /> */}
                     </div>
                 </div>
             </header>
